367e4feefece5e919b9e29455b0672a0ecc3cfc8
tnahddisttud <siddhantp73@gmail.com>
2021-10-01 22:30:22 +0530

    new commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(__dirname + "/dist/smart-login"));
+
+app.get("/*", (req, res, next) => {
+  res.sendFile(path.join(__dirname + "/dist/dictionary/index.html"));
+});
+
+app.listen(process.env.PORT || 8000);
