ee82f0e4c08195869ea6e293ff7fd510ee0a6fba
tnahddisttud <siddhantp73@gmail.com>
2021-10-01 22:43:13 +0530

    Another commit hj

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+//Install express server
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
-app.use(express.static(__dirname + "/dist/smart-login"));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + "/dist/smarttask-login"));
 
-app.get("/*", (req, res, next) => {
-  res.sendFile(path.join(__dirname + "/dist/dictionary/index.html"));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/smarttask-login/index.html"));
 });
 
-app.listen(process.env.PORT || 8000);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
