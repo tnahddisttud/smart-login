a89d9b252106ca3969de5bc9d922da249b61eadf
tnahddisttud <siddhantp73@gmail.com>
2021-10-02 11:43:32 +0530

    Deploy to heroku

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/smarttask-login"));
+app.use(express.static(__dirname + "/dist/smart-login"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/smarttask-login/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/smart-login/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
